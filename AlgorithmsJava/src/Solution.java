b7bed080ba7d7334daed16274d1c7918a1e907b8
batuuqur <batuuqur@hotmail.com>
2025-06-14 19:38:41 -0400

    first algorithm

---
+++ AlgorithmsJava/src/Solution.java
@@ -0,0 +1,50 @@
+// for (int i = 0; i < s.length(); i++) {
+//     // odd
+//     // even
+//     // update start & end
+// }
+// return s.substring(start, end + 1);
+
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+
+        int start = 0, end = 0;
+
+        for(int i=0; i < s.length(); i++)
+        {
+            int left = i, right = i;
+            //odd
+            while(left >= 0 && right < s.length() && s.charAt(left)==s.charAt(right))
+            {
+                left--;
+                right++;
+            }
+            int len1 = right-left-1;
+
+            left = i;
+            right = i + 1;
+            //even
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right))
+            {
+                left--;
+                right++;
+            }
+            int len2 = right - left - 1;
+
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String input = "babad";
+        String result = sol.longestPalindrome(input);
+        System.out.println("Longest Palindromic Substring: " + result);
+    }
+}
